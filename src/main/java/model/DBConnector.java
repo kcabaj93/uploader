1d662a41e56b93544edbbf1dcb8af99d71bb96d8
Kamil Cabaj <k.cabaj@globalsystem.pl>
2021-02-20 19:54:36 +0100

    podmiana stalych danych do DB na konfgurowalne z pliku propertis

--- src/main/java/model/DBConnector.java
+++ src/main/java/model/DBConnector.java
@@ -1,15 +1,13 @@
 package model;
 
+import controller.GetPropertyValues;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-    private final static String DBURL = "jdbc:mysql://127.0.0.1:3306/loader";
-    private final static String DBUSER = "root";
-    private final static String DBPASS = "";
-    private final static String DBDRIVER = "com.mysql.jdbc.Driver";
-
     private Connection connection;
 
     /**
@@ -18,9 +16,12 @@ public class DBConnector {
      * @return
      */
     public Connection connect() {
+        GetPropertyValues getPropertyValues = new GetPropertyValues();
         try {
-            connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
-        } catch (SQLException e) {
+            connection = DriverManager.getConnection(getPropertyValues.getPropValues("DBURL"),
+                    getPropertyValues.getPropValues("DBUSER"),
+                    getPropertyValues.getPropValues("DBPASS"));
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
         return connection;
